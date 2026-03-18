001ded9fdc1c47b1c0656a4ca686975ddd2665d7
daniel ukuhor <dani3lsavag3@gmail.com>
2026-03-18 05:13:54 +0100

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// what does the ui look like here and i wonder if there is some sort of anonymous block for comments just like the web or html page
